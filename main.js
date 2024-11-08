90a20d7025750c4ed67bac34f2103798ad8fa4d4
Marcus <reveriejacket@hotmail.com>
2024-11-08 17:07:36 -0300

    Projetos adicionados, mais dinamismo

--- main.js
+++ main.js
@@ -18,7 +18,7 @@ const addActiveClass = (entries, observer) =>{
 };
 
 const options = {
-    threshold: 0.8,
+    threshold: 0.9,
 };
 
 const observer = new IntersectionObserver(addActiveClass, options);
@@ -27,3 +27,22 @@ const sections = document.querySelectorAll("section");
 sections.forEach((section) => {
     observer.observe(section);
 });
+
+var revealer = document.querySelectorAll('.revealer span');
+
+window.onmousemove = function (e) {
+
+    for (var i = 0; i < revealer.length; i++) {
+        var tooltipImage = revealer[i].querySelector('img');
+            if (tooltipImage.id === 'eisen') {
+                x = (e.clientX + -300) + 'px';
+                y = (e.clientY + -200) + 'px';
+            } else if (tooltipImage.id === 'savesite') {
+                x = (e.clientX + -650) + 'px';
+                y = (e.clientY + -150) + 'px';
+            } 
+        revealer[i].style.top = y;
+        revealer[i].style.left = x;
+    }
+};
+
